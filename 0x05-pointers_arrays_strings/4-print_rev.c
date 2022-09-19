67d0ffc9f3fc42d047fe3203925dcb2cd5fff388
Your Name <you@example.com>
2022-09-19 13:52:08 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_rev - print a string in revers
+ *@s: a pointer given by the main function
+ *Return: 0
+ *
+*/
+
+void print_rev(char *s)
+{
+int counter = 0;
+while (s[counter] != '\0')
+counter++;
+while (counter)
+{
+_putchar(s[counter - 1]);
+counter--;
+}
+_putchar(10);
+}
