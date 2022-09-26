7f6ad1c8e8d58c7e8f08454fb93a9d21205458be
Your Name <you@example.com>
2022-09-26 14:19:02 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -6,17 +6,18 @@
  *@accept: a string given by main
  *Return: a pointer to s
 */
-
 char *_strpbrk(char *s, char *accept)
 {
 int i;
-int j
+int j;
+
 for (i = 0; s[i] != 0; i++)
 {
 for (j = 0; accept[j] != 0; j++)
 if (s[i] == accept[j])
 return (s + i);
 }
-return (0);
-}
 
+return (0);
+
+}
