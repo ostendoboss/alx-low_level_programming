ea2a24fe55442b57682025098957dbf698be2cb8
Your Name <you@example.com>
2022-10-03 14:44:16 -0700

    task 3

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -5,9 +5,9 @@
 /**
  *str_concat - concatenates two strings
  *@s1: string 1
- *s2: string 2
+ *@s2: string 2
  *Return: pointer to concatenated string
-*/ 
+*/
 
 char *str_concat(char *s1, char *s2)
 {
@@ -23,7 +23,7 @@ while (*(s2 + j))
 len2++, j++;
 len2++; /* add null terminator to length */
 concat = malloc(sizeof(char) * (len1 + len2)); /*alloc memory*/
-if (concat == NULL) /* validate memory 
+if (concat == NULL) /* validate memory */
 return (NULL);
 i = 0, j = 0;
 while (i < len1) /* concatenate */
@@ -38,4 +38,5 @@ i++, j++;
 }
 return (concat);
 }
+
 
