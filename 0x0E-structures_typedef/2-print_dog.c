feeef598e24545167c51de870c5155c032266346
Your Name <you@example.com>
2022-10-10 15:30:52 -0700

    task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,12 +16,12 @@ return;
 }
 else
 {
-if (d->name == NULL) 
+if (d->name == NULL)
 printf("Name: (nil)\n");
 else
 printf("Name: %s\n", d->name);
 printf("Age: %f\n", d->age);
-if (d->owner == NULL) 
+if (d->owner == NULL)
 printf("Owner: (nil)\n");
 else
 printf("Owner: %s\n", d->owner);
