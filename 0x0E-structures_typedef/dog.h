f4d3d6fdc1b56dcb1e9b77f017d17b0f682b43f2
Your Name <you@example.com>
2022-10-10 14:21:29 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,26 +1,24 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef STRTYP
+#define STRTYP
 
 /**
- *struct dog - struct for dog
- *@name: name
- *@age: age
- *@owner: owner
- *Description: This struct is for dog
+ *struct dog - structure of a dog
+ *@name: name of a dog
+ *@age: age of a dog
+ *@owner: owner of a dog
 */
-
 struct dog
-
 {
 char *name;
 float age;
 char *owner;
 };
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif/*DOG_H*/
+#endif /*STRTYP*/
 
