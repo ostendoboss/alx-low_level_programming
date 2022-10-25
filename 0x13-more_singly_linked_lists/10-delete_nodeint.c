ecb23d878e0895c0fdbe4d888ee1cb79959f77b7
Your Name <you@example.com>
2022-10-25 03:11:21 -0700

    this commit

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -18,12 +18,15 @@ if (index == 0)
 {
 *head = (*head)->next;
 free(temp);
-return (1);									}
+return (1);
+}
 while (i < index - 1)
-{										if (!temp || !(temp->next))
+{
+if (!temp || !(temp->next))
 return (-1);
 temp = temp->next;
-i++;										}
+i++;
+}
 current = temp->next;
 temp->next = current->next;
 free(current);
